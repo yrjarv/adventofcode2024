7aad42e851073f6af9d1228b9eebb558a4eb7d97
Yrjar Vederhus <yrjarvederhus@gmail.com>
2024-12-03 12:09:26 +0100

    Extract find and sum muls to function

--- day3/script.py
+++ day3/script.py
@@ -3,29 +3,28 @@ with open("input", "r") as file:
     for line in file:
         memory += line.strip()
 
+def mul(num1: int, num2: int) -> int:
+    return num1 * num2
+
+def find_and_sum_muls(mul_indexes: list[int], memory: str) -> int:
+    muls: list[str] = []
+    for index in mul_indexes:
+        if ")" not in memory[index:index+12]:
+            continue
+        endindex = index + memory[index:index+12].index(")")
+        muls.append(memory[index:endindex+1])
+    total = 0
+    for mul_string in muls:
+        try:
+            total += eval(mul_string)
+        except Exception as e:
+            continue
+    return total
+
 # Part 1
 mul_indexes: list[int] = []
 for i, _ in enumerate(memory[:-3]):
     if memory[i:i+4] != "mul(":
         continue
     mul_indexes.append(i)
-
-muls: list[str] = []
-for index in mul_indexes:
-    if ")" not in memory[index:index+12]:
-        continue
-    endindex = index + memory[index:index+12].index(")")
-    muls.append(memory[index:endindex+1])
-
-def mul(num1: int, num2: int):
-    return num1 * num2
-
-total = 0
-for mul_string in muls:
-    modified_string = "total += " + mul_string
-    try:
-        exec(modified_string)
-    except Exception as e:
-        continue
-
-print(f"Sum part 1: {total}")
+print(f"Sum part 1: {find_and_sum_muls(mul_indexes, memory)}")
