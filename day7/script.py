2cfa687b3129483d94e3746d65bb357af1ee8b9e
Yrjar Vederhus <yrjarvederhus@gmail.com>
2024-12-08 21:38:39 +0100

    Day 7 part 2

--- day7/script.py
+++ day7/script.py
@@ -1,33 +1,45 @@
 import itertools
 
-equations: dict[int, list[int]] = {}
-with open("input.txt", "r") as file:
-    for line in file:
-        line_content = line.strip().split(" ")
-        result = int(line_content[0][:-1]) # Remove colon
-        equations[result] = [int(element) for element in line_content[1:]]
+def main(operators: list[str]) -> None:
+    equations: list[tuple[int, list[int]]] = []
+    with open("input.txt", "r") as file:
+        for line in file:
+            line_content = line.strip().split(" ")
+            result = int(line_content[0][:-1]) # Remove colon
+            equations.append((result, [int(element) for element in line_content[1:]]))
 
-OPERATORS: list[str] = ["+", "*"]
+    sum_valid_equations = 0
+    for item in equations:
+        result, elements = item
+        possible_combinations = itertools.product(operators, repeat=len(elements)-1)
 
-sum_valid_equations = 0
-for result, elements in equations.items():
-    temporary_result = elements[0]
+        for combination in possible_combinations:
+            possible_result = elements[0]
 
-    possible_combinations = itertools.product(OPERATORS, repeat=len(elements)-1)
-    for combination in possible_combinations:
-        possible_result = elements[0]
+            equation = str(elements[0])
 
-        for i, operation in enumerate(combination):
-            match operation:
-                case "*":
-                    possible_result *= elements[i+1]
-                case "+":
-                    possible_result += elements[i+1]
-                case _:
-                    pass
+            for i, operation in enumerate(combination):
+                match operation:
+                    case "*":
+                        possible_result *= elements[i+1]
+                        equation += f" * {elements[i+1]}"
+                    case "+":
+                        possible_result += elements[i+1]
+                        equation += f" + {elements[i+1]}"
+                    case "||":
+                        equation += f" || {elements[i+1]}"
+                        possible_result = int(
+                            f"{possible_result}{elements[i+1]}"
+                        )
+                    case _:
+                        pass
+            
+            if possible_result == result:
+                sum_valid_equations += result
+                break
 
-        if possible_result == result:
-            sum_valid_equations += result
-            break
+    print(f"Sum with operators {operators}: {sum_valid_equations}")
 
-print(f"Sum of valid equations: {sum_valid_equations}")
+if __name__ == "__main__":
+    main(["+", "*"])
+    main(["+", "*", "||"])
